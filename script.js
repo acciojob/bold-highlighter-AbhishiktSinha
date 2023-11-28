db69ebcf796f5f2389ca715829edc60ef5c7824c
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-11-28 11:13:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
-
+	// get array of all <strong> elements
+	const boldElements = document.getElementsByTagName("strong");
+	for(let boldText of boldElements) {
+		boldText.style.color = "green";
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	const boldElements = document.getElementsByTagName("strong");
+	for(let boldText of boldElements) {
+		boldText.style.color = "#000000";
+	}
     
 }
